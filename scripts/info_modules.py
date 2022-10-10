1605fd3bc1602027127c2858ac3b64c6cb426061
Pedro Portocarrero <655306@soas.ac.uk>
2022-10-10 18:23:02 -0500

    Improvements on the code

--- scripts/info_modules.py
+++ scripts/info_modules.py
@@ -37,3 +37,73 @@ def fin_inf(nombre, ticker, period, interval, group_by):
 
     return y.to_feather(nombre)
 
+def fred_info(name, series_id):
+
+    import numpy as np
+    import requests
+    import pandas as pd
+    from datetime import date
+
+    with open('fred_key.txt', 'r') as file:
+
+        fred_key_str = file.read().rstrip()
+
+    url = 'https://api.stlouisfed.org/fred/series/observations?series_id=' + series_id + '&api_key=' + fred_key_str + '&file_type=json'
+
+    r = requests.get(url)
+
+    json_data = r.json()
+
+    df_data = pd.json_normalize(json_data, record_path=['observations'])
+
+    today = date.today()
+
+    today = pd.to_datetime(today)
+
+    df_data['date'] = pd.to_datetime(df_data['date'])
+
+    start = df_data['date'].searchsorted('2012-01-01')
+
+    end = df_data['date'].searchsorted(today)
+
+    df_data = df_data.iloc[start:end]
+
+    df_data = df_data.drop(columns=['realtime_start', 'realtime_end'])
+
+    df_data['value'].replace('.', np.nan, inplace=True)
+
+    df_data.dropna(subset=['value'], inplace=True)
+
+    return df_data.to_excel(name + '.xlsx')
+
+def fx_info(currency_1: str, currency_2: str, name: str):
+
+    import pandas as pd
+    import requests
+
+    with open('av_key.txt', 'r') as file:
+
+        av_key_str = file.read().rstrip()
+
+    url = 'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=' + currency_1 + '&to_symbol=' + currency_2 + '&outputsize=full&apikey=' + av_key_str + '&datatype=json'
+
+    param_headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+                      }
+
+    r = requests.get(url, headers=param_headers)
+
+    data = r.json()
+
+    data_df = pd.DataFrame(data['Time Series FX (Daily)']).T
+
+    data_df.index.name = 'Date'
+
+    data_df = data_df.rename(columns={'4. close': 'Close'})
+
+    data_df.reset_index()
+
+    data_df.sort_values(by='Date', inplace=True)
+
+    return data_df.to_excel(name + '.xlsx')
+
+
